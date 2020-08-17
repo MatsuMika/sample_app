3d4573119fb1849204854f4254617a27c31e12a1
松永実華 <mika@matsunagamikanoMacBook-Pro.local>
2020-08-17 22:30:39 +0900

    Rails11章Finish

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '9b55ddcbf0b300acc8b99ebf9b04555cfa35ffc0c7810652bea383e966e1940ba0ef13a9353f7241ad6c7c15da7e8e3c94fbe9c611af535aa6e78cafa860b0ae'
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_091050) do
+ActiveRecord::Schema.define(version: 2020_08_17_112700) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
   end
 
 end
